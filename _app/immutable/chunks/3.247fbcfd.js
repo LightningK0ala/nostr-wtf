3caa09a4d9bfb77ab3133ed1991213f2a7fa69d2
Lightning K0ala <lightningk0ala@protonmail.com>
2023-03-20 22:40:55 +0000

    Updates

--- _app/immutable/chunks/3.247fbcfd.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/query-page.svelte.7be6544a.js";export{t as component};
---
+++ _app/immutable/chunks/3.5d69f79d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/query-page.svelte.a25d5d1b.js";export{t as component};
